be23099de406fd93235adfdc6df03cebb552e2c0
Aleksey Filippov <sarum9in@gmail.com>
2015-09-21 01:41:47 +0400

    Implemented generator.config.ParseConfig().

--- generator/config/config.go
+++ generator/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+    "encoding/json"
+    "os"
+)
+
 type Config struct {
     LocalImportPrefix string
 }
@@ -9,6 +14,16 @@ func NewConfig() *Config {
 }
 
 func ParseConfig(path string) (cfg *Config, err error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return
+    }
+    defer file.Close()
+    decoder := json.NewDecoder(file)
     cfg = NewConfig()
+    err = decoder.Decode(cfg)
+    if err != nil {
+        cfg = nil
+    }
     return
 }
