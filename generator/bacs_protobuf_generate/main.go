9ca5b59e190be72bcbfbdf69b7b62ae080b192f7
Aleksey Filippov <sarum9in@gmail.com>
2015-09-22 16:06:11 +0400

    Migrated to flag.

--- generator/bacs_protobuf_generate/main.go
+++ generator/bacs_protobuf_generate/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
+    "flag"
     "github.com/bacsorg/protobuf/generator"
-    "gopkg.in/codegangsta/cli.v1"
     "log"
-    "os"
 )
 
 func main() {
-    app := cli.NewApp()
-    app.Name = "Google Protobuf generator"
-    app.Usage = "Call from go generate to update Go protobufs"
-    app.Action = func(c *cli.Context) {
-        err := generator.Generate()
-        if err != nil {
-            log.Fatal(err)
-        }
+    flag.Parse()
+    err := generator.Generate()
+    if err != nil {
+        log.Fatal(err)
     }
-    app.Run(os.Args)
 }
